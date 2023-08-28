201aba8e2ebd02047d90b38e784047fd2f7e7702
Pranalijunawane07 <pranalijunawane@gmail.com>
2023-08-28 15:37:12 +0530

    file1

---
+++ WhileLoop.js
@@ -0,0 +1,74 @@
+//Syntax
+
+//intiliazation
+
+//while(){
+// statements 
+// increment/ decrement
+//}
+
+//Program 1 
+
+// let p1 = 1
+// while (p1 <= 7) {
+//     console.log("hello")// hello,hello,hello,hello,hello,hello,hello
+//     p1++ // 2 ,3,4,5,6,7,8
+// }
+
+//Program 2
+
+// let P2 = 1
+// while (P2 <= 5) {
+//     console.log(P2)
+//     P2++
+// }
+
+//program 3
+
+//let p3 = 2
+// while (p3 <= 20) {
+//     console.log(p3)
+//     p3 = p3 + 2
+// }
+
+
+//Using break statment 
+
+// let p4 = 6
+// while (p4 >= 1) {
+//     console.log(p4)
+//     if (p4 == 4) {
+//         break
+//     }
+//     p4--
+// }
+
+
+//Using continue statment 
+
+
+// let p5 = 1
+// while (p5 >= 6) {
+//     console.log(p5)
+//     if (p5 == 4) {
+//         p5++
+//         continue
+//     }
+//     console.log(p5)
+//     p5++
+// }
+
+
+let i9 = 1
+while(i9 <= 8){
+    console.log('Yash')
+    i9++
+}
+ 
+
+
+
+
+
+
+
