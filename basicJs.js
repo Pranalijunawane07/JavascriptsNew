ab63201281fa7f8cd8d3ffe983527d68dec142ce
Pranalijunawane07 <pranalijunawane@gmail.com>
2023-08-30 13:02:03 +0530

    second

---
+++ basicJs.js
@@ -0,0 +1,30 @@
+let p = "pranali"
+console.log(p)
+
+//function with parameter and without return type 
+
+function add(x, y) {
+    console.log(x + y)
+
+}
+
+add(10, 34)
+
+// function with parameter and with return type
+function sub(p, q) {
+    return p - q
+}
+let mul = sub(45,67)
+console.log(mul)
+
+
+
+// function addC(x, y) {
+//     return x + y
+// }
+// let q3 = addC(3, 2)
+// console.log(q3)
+// console.log(q3 + q3)
+// console.log(q3 - 2)
+// console.log(q3 * 0.10)
+// console.log(q3 / 5)
